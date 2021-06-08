d5120528eaf6f9cea25c3df3d5e88fa10a74611f
jinqingsong <1211050148@qq.com>
2021-06-08 11:22:39 +0800

    优化

--- src/components/entranceGuard/index.jsx
+++ src/components/entranceGuard/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.scss";
 import "./style.scss";
 import { useDispatch, useMappedState } from "redux-react-hook";
 
@@ -10,6 +12,24 @@ function Entranceguard() {
     { name: "点位用途", value: "通威大道西街道" },
     { name: "所在位置", value: "信息监察部门" },
     { name: "所属部门", value: "人脸抓拍" },
+  ];
+  const num = "0528";
+  const peopleContent = [
+    {
+      pic: "tou",
+      name: "张丽丽",
+      time: "19:09:32",
+    },
+    {
+      pic: "tou",
+      name: "张丽丽",
+      time: "19:09:32",
+    },
+    {
+      pic: "tou",
+      name: "张丽丽",
+      time: "19:09:32",
+    },
   ];
   useEffect(() => {}, [top_count]);
 
@@ -26,6 +46,39 @@ function Entranceguard() {
             </div>
           );
         })}
+      </div>
+      <div className="numbox">
+        <span className="num_top">今日累计通行人数</span>
+        <span className="num_bot">{num}</span>
+      </div>
+      <div className="pic_con">
+        <Swiper
+          spaceBetween={100}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {peopleContent.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <div className="picbox">
+                  <img
+                    className="pic_icon"
+                    src={
+                      require("../../assets/tongwei/" + item.pic + ".png")
+                        .default
+                    }
+                    alt=""
+                  />
+                  <div className="pic_content">
+                    <div className="pic_name">{item.name}</div>
+                    <div className="pic_time">{item.time}</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
