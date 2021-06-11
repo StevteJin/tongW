80733e1cf748676abfa64ccc527c18c76808158e
jinqingsong <1211050148@qq.com>
2021-06-11 13:45:47 +0800

    今日报警类型统计

--- src/components/plx/index.jsx
+++ src/components/plx/index.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./style.scss";
 import { useDispatch, useMappedState } from "redux-react-hook";
-import { Progress } from 'antd';
+import { Progress } from "antd";
 
 function Plx() {
   const top_count = useMappedState((state) => state.top_navigation_count);
 
   const title = "今日报警类型统计";
-  const fenleiData = [
-    { name: "陌生人", value: "18" },
-    { name: "访客非法闯入", value: "14" },
-    { name: "未带口罩", value: "11" },
-    { name: "翻越闸机", value: "11" },
-    { name: "踩草坪", value: "8" },
-    { name: "未戴安全帽", value: "5" },
-    { name: "打架斗殴", value: "3" },
-    { name: "物品滞留", value: "2" },
-    { name: "其他", value: "1" },
+  const plxData = [
+    { name: "陌生人", value: "18", pic: "plx_h" },
+    { name: "访客非法闯入", value: "14", pic: "plx_h" },
+    { name: "未带口罩", value: "11", pic: "plx_h" },
+    { name: "翻越闸机", value: "11", pic: "plx_l" },
+    { name: "踩草坪", value: "8", pic: "plx_l" },
+    { name: "未戴安全帽", value: "5", pic: "plx_l" },
+    { name: "打架斗殴", value: "3", pic: "plx_l" },
+    { name: "物品滞留", value: "2", pic: "plx_l" },
+    { name: "其他", value: "1", pic: "plx_l" },
   ];
 
   useEffect(() => {}, [top_count]);
@@ -28,7 +28,28 @@ function Plx() {
         <span className="fen_title">{title}</span>
       </div>
       <div>
-
+        {plxData.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="plx_top">
+                <div className="plx_left">
+                  <img
+                    src={
+                      require("../../assets/tongwei/" + item.pic + ".png")
+                        .default
+                    }
+                    alt=""
+                  />
+                  <span>{item.name}</span>
+                </div>
+                <div className="plx_right">{item.value}</div>
+              </div>
+              <div className="plx_bot">
+                <Progress percent={30}  showInfo={false}/>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
