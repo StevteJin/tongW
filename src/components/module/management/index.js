70263871d902ee170e4d120cc9516a1e9cc5e55e
jinqingsong <1211050148@qq.com>
2021-06-11 11:00:48 +0800

    智慧管理

--- src/components/module/management/index.js
+++ src/components/module/management/index.js
@@ -1,5 +1,41 @@
+//智慧管理
 import React, { useRef, useState } from "react";
+//今日园区违纪统计
+import Wj from "../../wj";
+//今日人员违纪次数统计
+import Wjtimes from "../../wjtimes";
+//今日访客滞留统计
+import Zl from "../../zl";
+//今日园区人员巡更统计
+import Xg from "../../xg";
+//实时报警列表
+import Bj from "../../bj";
+
 import "./style.scss";
 export default function Management() {
-  return <div>3333</div>;
+  return (
+    <div className="perx">
+      <div className="perx_top">
+        <div className="perx_left">
+          <div className="wj">
+            <Wj />
+          </div>
+          <div className="wjtimes">
+            <Wjtimes />
+          </div>
+          <div className="zl">
+            <Zl />
+          </div>
+        </div>
+        <div className="perx_right">
+          <div className="xg">
+            <Xg />
+          </div>
+          <div className="bj">
+            <Bj />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
