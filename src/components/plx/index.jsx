f20267368356ee72596190d5e7f893d8a936300c
jinqingsong <1211050148@qq.com>
2021-06-15 16:03:26 +0800

    今日报警类型统计

--- src/components/plx/index.jsx
+++ src/components/plx/index.jsx
@@ -8,15 +8,15 @@ function Plx() {
 
   const title = "今日报警类型统计";
   const plxData = [
-    { name: "陌生人", value: "18", pic: "plx_h" },
-    { name: "访客非法闯入", value: "14", pic: "plx_h" },
-    { name: "未带口罩", value: "11", pic: "plx_h" },
-    { name: "翻越闸机", value: "11", pic: "plx_l" },
-    { name: "踩草坪", value: "8", pic: "plx_l" },
-    { name: "未戴安全帽", value: "5", pic: "plx_l" },
-    { name: "打架斗殴", value: "3", pic: "plx_l" },
-    { name: "物品滞留", value: "2", pic: "plx_l" },
-    { name: "其他", value: "1", pic: "plx_l" },
+    { name: "陌生人", value: "18", pic: "plx_h", ser: 1 },
+    { name: "访客非法闯入", value: "14", pic: "plx_h", ser: 2 },
+    { name: "未带口罩", value: "11", pic: "plx_h", ser: 3 },
+    { name: "翻越闸机", value: "11", pic: "plx_l", ser: 4 },
+    { name: "踩草坪", value: "8", pic: "plx_l", ser: 5 },
+    { name: "未戴安全帽", value: "5", pic: "plx_l", ser: 6 },
+    { name: "打架斗殴", value: "3", pic: "plx_l", ser: 7 },
+    { name: "物品滞留", value: "2", pic: "plx_l", ser: 8 },
+    { name: "其他", value: "1", pic: "plx_l", ser: 9 },
   ];
 
   useEffect(() => {}, [top_count]);
@@ -33,19 +33,15 @@ function Plx() {
             <div key={index}>
               <div className="plx_top">
                 <div className="plx_left">
-                  <img
-                    src={
-                      require("../../assets/tongwei/" + item.pic + ".png")
-                        .default
-                    }
-                    alt=""
-                  />
-                  <span>{item.name}</span>
+                  <div className={item.pic == "plx_h" ? "ser" : "noser"}>
+                    {item.ser}
+                  </div>
+                  <span className="num">{item.name}</span>
                 </div>
                 <div className="plx_right">{item.value}</div>
               </div>
               <div className="plx_bot">
-                <Progress percent={30}  showInfo={false}/>
+                <Progress percent={30} showInfo={false} />
               </div>
             </div>
           );
