d1b5200f86e5a1224c9bd5bd55f6cb74fc31f651
jinqingsong <1211050148@qq.com>
2021-06-17 10:17:44 +0800

    地图地址配置文件

---
+++ src/api/config.js
@@ -0,0 +1,5 @@
+const urlData = {
+  url55: "http://192.168.0.14:9917",
+  url50: "http://192.168.0.165:9901",
+};
+export default urlData;
--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
 import { createMap, Model } from "../../utils/map3d";
 
-import { Common } from '../../utils/mapMethods';
-import { useMappedState,useDispatch } from 'redux-react-hook';
-import { cameraList_S,labelLists } from '../../api/mainApi'
+import { Common } from "../../utils/mapMethods";
+import { useMappedState, useDispatch } from "redux-react-hook";
+import { cameraList_S, labelLists } from "../../api/mainApi";
+import urlData from "../../api/config";
 
 import "./style.scss";
 
 const Map = (props) => {
   const dispatch = useDispatch();
   const mapUrl = useMappedState((state) => state.mapLight_url);
+  console.log("我是地址", urlData);
   useEffect(() => {
-    console.log(mapUrl,'createMapcreateMapcreateMapcreateMapcreateMap')
+    console.log(mapUrl, "createMapcreateMapcreateMapcreateMapcreateMap");
     // if (mapUrl) {
     //   createMapsss("http://" + mapUrl);
     //   console.log('对象url',mapUrl)
@@ -19,20 +21,19 @@ const Map = (props) => {
     // eslint-disable-next-line
     createMapsss();
   }, []);
-    const createMapsss =(url)=>{
-        var map = createMap.createMap({
-            id: "mapv3dContainer_light",
-            url:"http://192.168.0.14:9917",
-            // url:"http://192.168.0.165:9901",
-            projectId: "5nbmjsdljf785208",
-            token: "rt2d645ty3eadaed32268mdta6"
-        },(()=>{
-
-        }))
-    }
-  return (
-    <div id="mapv3dContainer_light"></div>
-  );
+  const createMapsss = (url) => {
+    var map = createMap.createMap(
+      {
+        id: "mapv3dContainer_light",
+        url: urlData.url55,
+        // url: urlData.url50,
+        projectId: "5nbmjsdljf785208",
+        token: "rt2d645ty3eadaed32268mdta6",
+      },
+      () => {}
+    );
+  };
+  return <div id="mapv3dContainer_light"></div>;
 };
 
 export default Map;
