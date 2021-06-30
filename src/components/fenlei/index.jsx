791f50d7c986b7cf4d6bc961f604ca548693fc3a
jinqingsong <1211050148@qq.com>
2021-06-30 16:17:26 +0800

    各栋建组人数

--- src/components/fenlei/index.jsx
+++ src/components/fenlei/index.jsx
@@ -5,14 +5,14 @@ import { useDispatch, useMappedState } from "redux-react-hook";
 function Fenlei() {
   const top_count = useMappedState((state) => state.top_navigation_count);
 
-  const title = "今日人员分类统计";
+  const title = "各栋建组人数";
   const fenleiData = [
-    { name: "排班数", value: "4" },
-    { name: "在岗人数", value: "2,124" },
-    { name: "请假人数", value: "35" },
-    { name: "白班人数", value: "1624" },
-    { name: "夜班人数", value: "219" },
-    { name: "行政人数", value: "268" },
+    { name: "A1东区", value: "433" },
+    { name: "A1西区", value: "2,124" },
+    { name: "A1办公区", value: "353" },
+    { name: "食堂", value: "1624" },
+    { name: "动力站", value: "219" },
+    { name: "室外", value: "268" },
   ];
 
   useEffect(() => {}, [top_count]);
