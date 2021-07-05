69f8ff65fe81d79508cb272e804bdd1c17f8d2ce
jinqingsong <1211050148@qq.com>
2021-07-05 11:00:48 +0800

    公共样式

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import { Spin } from "antd";
 import moment from "moment";
 import "moment/locale/zh-cn";
 import "normalize.css";
-import "./index.css";
+import "./index.css";//公共样式
 import { LoadingOutlined } from "@ant-design/icons";
 const store = makeStore();
 moment.locale("zh-cn");
