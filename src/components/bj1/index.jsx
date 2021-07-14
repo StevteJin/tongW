a82f37c9496c2265ba67fe76f8eb1afa4212acfc
jinqingsong <1211050148@qq.com>
2021-07-14 13:53:00 +0800

    实时报警，车，时间跟随

--- src/components/bj1/index.jsx
+++ src/components/bj1/index.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useMappedState } from "redux-react-hook";
 
 function Bj1() {
   const top_count = useMappedState((state) => state.top_navigation_count);
+  const [timeNow, setTime] = useState();
 
   const title = "实时报警";
   const bjData = [
@@ -14,7 +15,7 @@ function Bj1() {
       snam: "就",
       pic: "carcar2",
       what: "未处理",
-      time: "2021-07-09 19:09:32",
+      time: timeNow,//时间需要跟随
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -25,7 +26,7 @@ function Bj1() {
       snam: "就",
       pic: "carcar1",
       what: "未处理",
-      time: "2021-07-09 18:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -36,7 +37,7 @@ function Bj1() {
       snam: "就",
       pic: "carcar3",
       what: "未处理",
-      time: "2021-07-09 18:19:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -47,7 +48,7 @@ function Bj1() {
       snam: "就",
       pic: "carcar4",
       what: "未处理",
-      time: "2021-07-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -58,7 +59,7 @@ function Bj1() {
       snam: "就",
       pic: "carcar5",
       what: "未处理",
-      time: "2021-07-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -69,13 +70,52 @@ function Bj1() {
       snam: "就",
       pic: "carcar6",
       what: "未处理",
-      time: "2021-07-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
   ];
 
-  useEffect(() => {}, [top_count]);
+  const showtime = () => {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    var time =
+      year +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds +
+      " "
+    setTime(time);
+    setTimeout(showtime, 1000);
+  };
+  useEffect(() => {showtime()}, [top_count]);
 
   return (
     <div className="bj_box">
