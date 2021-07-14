fb8cbd5455c39f7e6b2d93d0480eea61c1a6da53
jinqingsong <1211050148@qq.com>
2021-07-14 13:44:43 +0800

    车辆超速时间跟随

--- src/components/chaosu/index.jsx
+++ src/components/chaosu/index.jsx
@@ -4,16 +4,57 @@ import { useDispatch, useMappedState } from "redux-react-hook";
 
 function Chaosu() {
   const top_count = useMappedState((state) => state.top_navigation_count);
+  const [timeNow, setTime] = useState();
 
   const title = "车辆超速";
   const ffData = {
-    time: "2021-09-09 19:09:32",
+    time: timeNow,//时间需要跟随
     where: "园区西门1门BH19201TO",
     pic: "che",
     who: "川A·12345",
   };
 
-  useEffect(() => {}, [top_count]);
+  const showtime = () => {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    var time =
+      year +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds +
+      " "
+    setTime(time);
+    setTimeout(showtime, 1000);
+  };
+
+  useEffect(() => {showtime()}, [top_count]);
 
   return (
     <div className="fei_box">
