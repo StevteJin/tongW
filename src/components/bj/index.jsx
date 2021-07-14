09127f8f56267265c24eb45a0b1f525d4d541383
jinqingsong <1211050148@qq.com>
2021-07-14 13:39:42 +0800

    实时报警列表，时间跟随变动

--- src/components/bj/index.jsx
+++ src/components/bj/index.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useMappedState } from "redux-react-hook";
 
 function Bj() {
   const top_count = useMappedState((state) => state.top_navigation_count);
+  const [timeNow, setTime] = useState();
 
   const title = "实时报警列表";
   const bjData = [
@@ -11,7 +12,7 @@ function Bj() {
       name: "陌生人",
       pic: "tou",
       what: "未处理",
-      time: "2021-09-09 19:09:32",
+      time: timeNow,//时间需要跟随
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
@@ -19,13 +20,53 @@ function Bj() {
       name: "未戴口罩",
       pic: "tou",
       what: "已处理",
-      time: "2021-09-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
   ];
 
-  useEffect(() => {}, [top_count]);
+  const showtime = () => {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    var time =
+      year +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds +
+      " "
+    setTime(time);
+    setTimeout(showtime, 1000);
+  };
+
+  useEffect(() => {showtime()}, [top_count]);
 
   return (
     <div className="bj_boxx">
