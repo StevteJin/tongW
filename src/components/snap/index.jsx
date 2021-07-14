295151040c46c4c000d273ffb39dd78e70275ed6
jinqingsong <1211050148@qq.com>
2021-07-14 11:53:09 +0800

    实时人脸抓拍列表时间跟随

--- src/components/snap/index.jsx
+++ src/components/snap/index.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useMappedState } from "redux-react-hook";
 
 function Snap() {
   const top_count = useMappedState((state) => state.top_navigation_count);
+  const [timeNow, setTime] = useState();
 
   const title = "实时人脸抓拍列表";
   const snapData = [
@@ -11,14 +12,14 @@ function Snap() {
       name: "张学李(12452)",
       pic: "P4",
       what: "生产管理部门",
-      time: "2021-09-09 19:09:32",
+      time: timeNow,//这个时间需要跟随变动
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
     {
       name: "陌生人",
       pic: "P2",
-      time: "2021-09-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "no",
     },
@@ -26,13 +27,52 @@ function Snap() {
       name: "王学李(12452)",
       pic: "P3",
       what: "生产管理部门",
-      time: "2021-09-09 19:09:32",
+      time: timeNow,
       where: "园区西门1门BH19201",
       isHere: "yes",
     },
   ];
 
-  useEffect(() => {}, [top_count]);
+  const showtime = () => {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    var time =
+      year +
+      "-" +
+      month +
+      "-" +
+      day +
+      " " +
+      hours +
+      ":" +
+      minutes +
+      ":" +
+      seconds +
+      " "
+    setTime(time);
+    setTimeout(showtime, 1000);
+  };
+  useEffect(() => {showtime()}, [top_count]);
 
   return (
     <div className="snap_box">
