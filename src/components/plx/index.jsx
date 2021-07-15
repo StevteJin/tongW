9417756096f798e20f5c2bb17a12141278f05744
jinqingsong <1211050148@qq.com>
2021-07-15 10:28:43 +0800

    今日报警类型统计

--- src/components/plx/index.jsx
+++ src/components/plx/index.jsx
@@ -11,12 +11,12 @@ function Plx() {
     { name: "陌生人", value: "18", pic: "plx_h", ser: 1 },
     { name: "访客非法闯入", value: "14", pic: "plx_h", ser: 2 },
     { name: "未带口罩", value: "11", pic: "plx_h", ser: 3 },
-    { name: "翻越闸机", value: "11", pic: "plx_l", ser: 4 },
+    { name: "翻越闸机", value: "0", pic: "plx_l", ser: 4 },
     { name: "踩草坪", value: "8", pic: "plx_l", ser: 5 },
     { name: "未戴安全帽", value: "5", pic: "plx_l", ser: 6 },
-    { name: "打架斗殴", value: "3", pic: "plx_l", ser: 7 },
-    { name: "物品滞留", value: "2", pic: "plx_l", ser: 8 },
-    { name: "其他", value: "1", pic: "plx_l", ser: 9 },
+    // { name: "打架斗殴", value: "3", pic: "plx_l", ser: 7 },
+    { name: "物品滞留", value: "2", pic: "plx_l", ser: 7 },
+    { name: "其他", value: "1", pic: "plx_l", ser: 8 },
   ];
 
   useEffect(() => {}, [top_count]);
